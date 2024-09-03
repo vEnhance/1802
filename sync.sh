1cae3955d5c2b48baa04fa8483cb0fb7c0d8ab14
Evan Chen <evan@evanchen.cc>
2024-09-03 19:39:23 -0400

    chore: Script for syncing

---
+++ sync.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+readarray -t TARGET_FILES <targets.txt
+gsutil -m cp "${TARGET_FILES[@]}" gs://web.evanchen.cc/upload/1802/
+gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/upload/1802/**"
