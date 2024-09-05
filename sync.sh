48d5cb0dc708f11d15b7e8e3a4e695a35aa04934
Evan Chen <evan@evanchen.cc>
2024-09-04 21:39:11 -0400

    chore: Recompile Typst on sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+readarray -t TYPST_FILES < <(fd '.typ')
+for t in "${TYPST_FILES[@]}"; do
+  typst compile "$t"
+done
+
 readarray -t TARGET_FILES <targets.txt
 gsutil -m cp "${TARGET_FILES[@]}" gs://web.evanchen.cc/upload/1802/
 gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/upload/1802/**"
