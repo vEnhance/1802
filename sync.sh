5e813a0668de96b35632e33a09c1e0f159341387
Evan Chen <evan@evanchen.cc>
2024-09-07 11:37:41 -0400

    chore: Clean up targets sync script

--- sync.sh
+++ sync.sh
@@ -3,5 +3,12 @@ set -euxo pipefail
 
 make all
 readarray -t TARGET_FILES <targets.txt
-gsutil -m cp "${TARGET_FILES[@]}" gs://web.evanchen.cc/upload/1802/
-gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/upload/1802/**"
+
+for target in "${TARGET_FILES[@]}"; do
+  if [ ! -f outputs/"$(basename "$target")" ]; then
+    ln -s ../"$target" outputs/"$(basename "$target")"
+  fi
+done
+
+PYTHONWARNINGS="ignore" gsutil -m rsync outputs/ gs://web.evanchen.cc/upload/1802/
+PYTHONWARNINGS="ignore" gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/upload/1802/**"
