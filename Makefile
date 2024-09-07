396b259fd5adc3b72ec136f2cc72ac51e3a5069e
Evan Chen <evan@evanchen.cc>
2024-09-06 21:45:11 -0400

    feat: Makefile for both Typst and figures

---
+++ Makefile
@@ -0,0 +1,14 @@
+ASY_INPUTS := $(wildcard figures/*.asy)
+PNG_OUTPUTS := $(ASY_INPUTS:.asy=.png)
+TYP_INPUTS := $(wildcard *.typ)
+PDF_OUTPUTS := $(TYP_INPUTS:.typ=.pdf)
+
+all: $(PNG_OUTPUTS) $(PDF_OUTPUTS)
+
+figures/%.pdf: figures/%.asy
+	asy -f pdf $< -o $@
+figures/%.png: figures/%.pdf
+	magick -density 300 $< $@
+
+%.pdf: %.typ
+	typst compile $<
--- sync.sh
+++ sync.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-readarray -t TYPST_FILES < <(fd '.typ')
-for t in "${TYPST_FILES[@]}"; do
-  typst compile "$t"
-done
-
+make all
 readarray -t TARGET_FILES <targets.txt
 gsutil -m cp "${TARGET_FILES[@]}" gs://web.evanchen.cc/upload/1802/
 gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/upload/1802/**"
