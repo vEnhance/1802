41eff8e420cd8cbd85bd3bbfcd713a78f3582158
Evan Chen <evan@evanchen.cc>
2024-10-17 20:19:37 -0400

    chore: Migrate "gsutil" -> "gcloud storage"

--- sync.sh
+++ sync.sh
@@ -13,5 +13,5 @@ for target in "${TARGET_FILES[@]}"; do
   fi
 done
 
-PYTHONWARNINGS="ignore" gsutil -m rsync outputs/ gs://web.evanchen.cc/upload/1802/
-PYTHONWARNINGS="ignore" gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/upload/1802/**"
+gcloud storage rsync --no-ignore-symlinks outputs/ gs://web.evanchen.cc/upload/1802/
+gcloud storage objects update --recursive --cache-control="private,max-age=0" gs://web.evanchen.cc/upload/1802/
