aa9e5937714178006cdbdfe9dd9f55cc9e99777e
Evan Chen <evan@evanchen.cc>
2026-04-17 12:59:06 -0400

    build: symlink lamv.pdf at sync time

--- sync.sh
+++ sync.sh
@@ -3,5 +3,8 @@ set -euxo pipefail
 
 make all
 
+[[ -f lamv.pdf ]] || { echo "ERROR: lamv.pdf not found in top directory"; exit 1; }
+[[ -e published/lamv.pdf ]] || ln -s ../lamv.pdf published/lamv.pdf
+
 gcloud storage rsync --no-ignore-symlinks outputs/ gs://web.evanchen.cc/upload/1802 --cache-control="private,max-age=0"
 gcloud storage rsync --no-ignore-symlinks published/ gs://web.evanchen.cc/textbooks --cache-control="private,max-age=0"
